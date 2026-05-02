9839f53fc2b5ef58b7370897d61788469a4a28f8
nobstar <nwirges@gmx.de>
2026-05-02 15:06:27 +0200

    try to fix path for pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/trainingrandomizer/' : '/',
+  base: './',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
